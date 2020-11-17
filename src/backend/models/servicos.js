6979f451757a999ead4c789e698b4a28bf3aefc4
NunoRei <36609841+NunoRei@users.noreply.github.com>
2020-11-17 17:06:09 +0000

    Create servicos.js

---
+++ src/backend/models/servicos.js
@@ -0,0 +1,58 @@
+var mongoose = require('mongoose');
+
+// Services Schema
+var ServiceSchema = mongoose.Schema({
+	_id: {
+		
+    },
+    cliente: {
+        type: String
+    },
+    service_provider: {
+        type: String
+    },
+    urgente: {
+        type: Boolean,
+    },
+    status: {
+        type: Number,
+        required: true
+    },
+    fatura: {
+        type: Buffer
+    },
+    review: {
+        client: {
+            comentario: {
+                type: String
+            },
+            karma: {
+                type: Number
+            }
+        },
+        service_provider: {
+            comentario: {
+                type: String
+            },
+            karma: {
+                type: Number
+            }
+        }
+    },
+    orcamento: [
+        {
+            datetime: {
+                type: Date,
+                default: Date.now
+            },
+            user: {
+                type: String
+            },
+            value: { 
+                type: Number
+            }
+        }
+    ]
+});
+
+module.exports = mongoose.model('Service', UserSchema, 'services');
