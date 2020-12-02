8343a817f098df4ca8b948cbe97a6e29355ec892
Fábio Senra <fabio-senra@hotmail.com>
2020-12-02 18:56:07 +0000

    🤯🤯🤯🤯🤯
    gotta catch'em all
    
    Co-Authored-By: Pedro Gonçalves <pedrotgoncalves@hotmail.com>
    Co-Authored-By: NunoRei <36609841+NunoRei@users.noreply.github.com>
    Co-Authored-By: João Pimentel <32567686+citoplasme@users.noreply.github.com>
    Co-Authored-By: Jaime Leite <32790077+jaimeleite@users.noreply.github.com>

--- src/frontend/src/components/users/EditProfile.vue
+++ src/frontend/src/components/users/EditProfile.vue
@@ -9,19 +9,15 @@
               <v-img
                 v-if="
                   user.photo !== undefined &&
-                    user.photo.content !== undefined &&
-                    user.photo.extension !== undefined
+                  user.photo.content !== undefined &&
+                  user.photo.extension !== undefined
                 "
-                :src="
-                  `data:image/${user.photo.extension};base64,${
-                    user.photo.content
-                  }`
-                "
-                style="width:100%; height:100%;"
+                :src="`data:image/${user.photo.extension};base64,${user.photo.content}`"
+                style="width: 100%; height: 100%"
               />
               <v-img
                 v-else
-                style="width:100%; height:100%;"
+                style="width: 100%; height: 100%"
                 :src="require('@/assets/default_user.png')"
               />
             </v-list-item-avatar>
@@ -29,7 +25,7 @@
               <div
                 v-if="hover"
                 class="d-flex transition-fast-in-fast-out darken-2 v-card--reveal display-3 white--text"
-                style="height: 100%;"
+                style="height: 100%"
               >
                 <v-tooltip bottom>
                   <template v-slot:activator="{ on }">
@@ -43,8 +39,8 @@
                   bottom
                   v-if="
                     user.photo !== undefined &&
-                      user.photo.content !== undefined &&
-                      user.photo.extension !== undefined
+                    user.photo.content !== undefined &&
+                    user.photo.extension !== undefined
                   "
                 >
                   <template v-slot:activator="{ on }">
@@ -68,9 +64,9 @@
       <v-list-item
         v-if="
           user.level >= 3 &&
-            user.level <= 4 &&
-            user.categorias &&
-            user.categorias.length > 0
+          user.level <= 4 &&
+          user.categorias &&
+          user.categorias.length > 0
         "
       >
         <v-col cols="2">
@@ -92,9 +88,9 @@
       <v-list-item
         v-if="
           user.level >= 3 &&
-            user.level <= 4 &&
-            user.subcategorias &&
-            user.subcategorias.length > 0
+          user.level <= 4 &&
+          user.subcategorias &&
+          user.subcategorias.length > 0
         "
       >
         <v-col cols="2">
@@ -118,9 +114,9 @@
       <v-list-item
         v-if="
           user.level >= 3 &&
-            user.level <= 4 &&
-            user.locations &&
-            user.locations.length > 0
+          user.level <= 4 &&
+          user.locations &&
+          user.locations.length > 0
         "
       >
         <v-col cols="2">
@@ -393,7 +389,7 @@ import Loading from "@/components/generic/Loading";
 import querystring from "querystring";
 export default {
   components: {
-    Loading
+    Loading,
   },
   data: () => ({
     user: {},
@@ -409,23 +405,23 @@ export default {
     ready: false,
     categories: [],
     specializations: [],
-    regraNome: [v => !!v || "Name is required."],
-    regraImagem: [v => !!v || "Image is required."],
+    regraNome: [(v) => !!v || "Name is required."],
+    regraImagem: [(v) => !!v || "Image is required."],
     regraEmail: [
-      v => !!v || "Email is required.",
-      v => /^.+@.+\..+$/.test(v) || "Email has to be valid."
+      (v) => !!v || "Email is required.",
+      (v) => /^.+@.+\..+$/.test(v) || "Email has to be valid.",
     ],
-    regraTipo: [v => !!v || "User type is required."],
-    regraPassword: [v => !!v || "Password is required."],
+    regraTipo: [(v) => !!v || "User type is required."],
+    regraPassword: [(v) => !!v || "Password is required."],
     editedItem: {},
-    ficheiro: {},
+    ficheiro: [],
     dialog_image_delete: false,
     dialog_categories: false,
     newcategories: [],
     searchString: [],
     dialog_specializations: false,
     searchString2: [],
-    newspecializations: []
+    newspecializations: [],
   }),
   async created() {
     var res = await this.$request(
@@ -439,12 +435,12 @@ export default {
     this.ready = true;
   },
   methods: {
-    preparaCampos: async function(array) {
+    preparaCampos: async function (array) {
       try {
-        let res = array.map(v => {
+        let res = array.map((v) => {
           return {
             text: v.name,
-            value: v._id
+            value: v._id,
           };
         });
         return res;
@@ -453,23 +449,23 @@ export default {
       }
     },
     async editar_specializations() {
-      let specs = this.specializations.filter(el =>
-        this.user.categorias.find(e => e._id === el.category)
+      let specs = this.specializations.filter((el) =>
+        this.user.categorias.find((e) => e._id === el.category)
       );
       this.newspecializations = await this.preparaCampos(specs);
-      this.searchString2 = this.user.subcategorias.map(x => x._id);
+      this.searchString2 = this.user.subcategorias.map((x) => x._id);
       this.dialog_specializations = true;
     },
     async editar_categories() {
       this.newcategories = await this.preparaCampos(this.categories);
-      this.searchString = this.user.categorias.map(x => x._id);
+      this.searchString = this.user.categorias.map((x) => x._id);
       this.dialog_categories = true;
     },
     remover_imagem(item) {
       this.dialog_image_delete = true;
     },
     editar_imagem(item) {
-      this.ficheiro = {}; //Object.assign({}, this.user.photo);
+      //this.ficheiro = {}; //Object.assign({}, this.user.photo);
       this.dialog_image = true;
     },
     editar(item) {
@@ -478,12 +474,12 @@ export default {
     },
     filter_query_string(ids) {
       let obj = {
-        _id: ids
+        _id: ids,
       };
       let new_qs = querystring.stringify(obj);
       return new_qs;
     },
-    go: function(url) {
+    go: function (url) {
       if (url.startsWith("http")) {
         window.location.href = url;
       } else {
@@ -493,13 +489,13 @@ export default {
     async merge_fileds() {
       try {
         // FILTRAR ACTIVES ???
-        let cats = this.user.categorias.map(c => {
-          return this.categories.find(obj => obj._id === c);
+        let cats = this.user.categorias.map((c) => {
+          return this.categories.find((obj) => obj._id === c);
         });
 
         // FILTRAR ACTIVES ???
-        let specs = this.user.subcategorias.map(sc => {
-          return this.specializations.find(obj => obj._id === sc);
+        let specs = this.user.subcategorias.map((sc) => {
+          return this.specializations.find((obj) => obj._id === sc);
         });
 
         this.user.categorias = cats;
@@ -530,8 +526,8 @@ export default {
           let qs = this.filter_query_string(this.user.locations);
           let queryS = qs === "" ? "" : "?" + qs;
           var response = await this.$request("get", "/locations/" + queryS);
-          let specs = this.user.locations.map(sc => {
-            return response.data.find(obj => obj._id === sc);
+          let specs = this.user.locations.map((sc) => {
+            return response.data.find((obj) => obj._id === sc);
           });
           this.user.locations = specs;
         }
@@ -560,9 +556,9 @@ export default {
         this.$request("put", "/users/" + this.editedItem._id, {
           nome: this.editedItem.name,
           email: this.editedItem.email,
-          level: this.user.level
+          level: this.user.level,
         })
-          .then(res => {
+          .then((res) => {
             this.text = res.data;
             this.color = "success";
             this.snackbar = true;
@@ -570,7 +566,7 @@ export default {
             this.dialog = false;
             this.getUser();
           })
-          .catch(err => {
+          .catch((err) => {
             this.text = err.response.data;
             this.color = "error";
             this.snackbar = true;
@@ -595,7 +591,7 @@ export default {
           "/users/" + this.user._id + "/uploadphoto",
           formData
         )
-          .then(res => {
+          .then((res) => {
             this.text = res.data;
             this.color = "success";
             this.snackbar = true;
@@ -603,7 +599,7 @@ export default {
             this.dialog_image = false;
             this.getUser();
           })
-          .catch(err => {
+          .catch((err) => {
             this.text = err.response.data;
             this.color = "error";
             this.snackbar = true;
@@ -619,9 +615,9 @@ export default {
     async guardar_categories() {
       if (this.$refs.form_categories.validate()) {
         this.$request("put", "/users/" + this.user._id + "/categories", {
-          categories: this.searchString
+          categories: this.searchString,
         })
-          .then(res => {
+          .then((res) => {
             this.text = res.data;
             this.color = "success";
             this.snackbar = true;
@@ -629,7 +625,7 @@ export default {
             this.dialog_categories = false;
             this.getUser();
           })
-          .catch(err => {
+          .catch((err) => {
             this.text = err.response.data;
             this.color = "error";
             this.snackbar = true;
@@ -645,9 +641,9 @@ export default {
     async guardar_specializations() {
       if (this.$refs.form_specializations.validate()) {
         this.$request("put", "/users/" + this.user._id + "/specializations", {
-          specializations: this.searchString2
+          specializations: this.searchString2,
         })
-          .then(res => {
+          .then((res) => {
             this.text = res.data;
             this.color = "success";
             this.snackbar = true;
@@ -655,7 +651,7 @@ export default {
             this.dialog_specializations = false;
             this.getUser();
           })
-          .catch(err => {
+          .catch((err) => {
             this.text = err.response.data;
             this.color = "error";
             this.snackbar = true;
@@ -670,7 +666,7 @@ export default {
     },
     async eliminar_imagem() {
       await this.$request("delete", "/users/" + this.user._id + "/removephoto")
-        .then(res => {
+        .then((res) => {
           this.text = res.data;
           this.color = "success";
           this.snackbar = true;
@@ -678,7 +674,7 @@ export default {
           this.dialog_image_delete = false;
           this.getUser();
         })
-        .catch(err => {
+        .catch((err) => {
           this.text = err.response.data;
           this.color = "error";
           this.snackbar = true;
@@ -688,8 +684,8 @@ export default {
     fecharSnackbar() {
       this.snackbar = false;
       if (this.done == true) this.getUser();
-    }
-  }
+    },
+  },
 };
 </script>
 
