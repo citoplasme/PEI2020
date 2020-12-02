432f27e4e73d4f962d3b581e7a246207efcaf8d2
citoplasme <jony.pi10@gmail.com>
2020-12-02 14:49:15 +0000

    Update rota de PUT :id users

--- src/backend/routes/api/users.js
+++ src/backend/routes/api/users.js
@@ -260,19 +260,37 @@ router.put('/:id/password', Auth.isLoggedInUser, function (req, res) {
 });
 
 
-router.put('/:id', Auth.isLoggedInUser, Auth.checkLevel(5), function (req, res) {
+router.put('/:id', Auth.isLoggedInUser, Auth.checkLevel(1), function (req, res) {
     Users.getUserByEmail(req.body.email, function(err,user){
+        // If user that has that email is not the one editing 
         if(user && req.params.id != user._id){
             res.status(500).send('It was not possible to update the user. The email is already in use.');
         }else{
-            Users.atualizarMultiplosCampos(req.params.id, req.body.nome, req.body.email, req.body.level, function (err, cb) {
-                if (err) { 
-                    //res.status(500).send(`Erro: ${err}`);
-                    res.status(500).send('It was not possible to update the user. Please verify if all the fields are filled.');
-                } else {
-                    res.send('User updated with success.')
-                }
-            });
+            // Check if admin
+            if(req.user.level >= 5){
+                Users.atualizarMultiplosCampos(req.params.id, req.body.nome, req.body.email, req.body.level, function (err, cb) {
+                    if (err) { 
+                        //res.status(500).send(`Erro: ${err}`);
+                        res.status(500).send('It was not possible to update the user. Please verify if all the fields are filled.');
+                    } else {
+                        res.send('User updated with success.')
+                    }
+                });
+            } 
+            // If user is himself
+            else if(req.user.id == req.params.id){
+                Users.atualizarMultiplosCampos(req.params.id, req.body.nome, req.body.email, req.body.level, function (err, cb) {
+                    if (err) { 
+                        //res.status(500).send(`Erro: ${err}`);
+                        res.status(500).send('It was not possible to update the user. Please verify if all the fields are filled.');
+                    } else {
+                        res.send('User updated with success.')
+                    }
+                });
+            }
+            else {
+                res.status(500).send('It was not possible to update the user: you cannot update an user that is not yourself.');
+            }
         }
     });
 });
