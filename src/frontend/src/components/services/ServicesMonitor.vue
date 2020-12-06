e02e221e8fb4669f20c33f5b0ff452d7ea71ba73
JoaoCBFreitas <joaobcbf@hotmail.com>
2020-12-06 16:21:54 +0000

    Gráfico de valores por serviço

--- src/frontend/src/components/services/ServicesMonitor.vue
+++ src/frontend/src/components/services/ServicesMonitor.vue
@@ -1,82 +1,186 @@
 <template>
   <v-content>
-    <Loading v-if="!ready" :message="'the service\'s management info'" />
-    <v-container fluid grid-list-md v-else>
-      <v-layout row wrap>
-        <v-flex
-          md3
-          sm6
-          xs12
-          full
-          v-for="(stat, index) in stats"
-          v-bind:key="index"
-        >
-          <v-card :class="stat.bgColor" dark>
-            <v-container fluid grid-list-sm dark>
-              <v-layout class="mt-0 mb-0 mx-0" row wrap>
-                <v-flex sm3 hidden-xs-only>
-                  <v-icon class="mx-0" x-large dark>{{ stat.icon }}</v-icon>
-                </v-flex>
-                <v-flex sm9 xs12>
-                  <v-layout class="mt-0 mb-0 pa-0" row wrap>
-                    <v-flex d-flex xs12>
-                      <div class="silver--text subheading">
-                        {{ stat.title }}
-                      </div>
+    <v-row>
+      <v-col>
+        <Loading v-if="!ready" :message="'the service\'s management info'" />
+        <v-container fluid grid-list-md v-else>
+          <v-layout row wrap>
+            <v-flex
+              md3
+              sm6
+              xs12
+              full
+              v-for="(stat, index) in stats"
+              v-bind:key="index"
+            >
+              <v-card :class="stat.bgColor" dark>
+                <v-container fluid grid-list-sm dark>
+                  <v-layout class="mt-0 mb-0 mx-0" row wrap>
+                    <v-flex sm3 hidden-xs-only>
+                      <v-icon class="mx-0" x-large dark>{{ stat.icon }}</v-icon>
                     </v-flex>
-                    <v-flex d-flex xs12 class="mx-4">
-                      <div class="silver--text display-1">{{ stat.data }}</div>
-                      &nbsp;&nbsp;
-                      <v-btn
-                        outlined
-                        class="darkgrey--text darken-1"
-                        v-if="stat.action.label.length > 0"
-                        right
-                        text
-                        small
-                        @click="go(stat.action.link)"
-                      >
-                        {{ stat.action.label }}
-                      </v-btn>
+                    <v-flex sm9 xs12>
+                      <v-layout class="mt-0 mb-0 pa-0" row wrap>
+                        <v-flex d-flex xs12>
+                          <div class="silver--text subheading">
+                            {{ stat.title }}
+                          </div>
+                        </v-flex>
+                        <v-flex d-flex xs12 class="mx-4">
+                          <div class="silver--text display-1">
+                            {{ stat.data }}
+                          </div>
+                          &nbsp;&nbsp;
+                          <v-btn
+                            outlined
+                            class="darkgrey--text darken-1"
+                            v-if="stat.action.label.length > 0"
+                            right
+                            text
+                            small
+                            @click="go(stat.action.link)"
+                          >
+                            {{ stat.action.label }}
+                          </v-btn>
+                        </v-flex>
+                      </v-layout>
                     </v-flex>
                   </v-layout>
-                </v-flex>
-              </v-layout>
-            </v-container>
-          </v-card>
-        </v-flex>
-      </v-layout>
-      <v-layout class="" row wrap>
-        <v-flex md6 xs12>
-          <v-card light>
-            <v-container>
-              <bar></bar>
-            </v-container>
-          </v-card>
-        </v-flex>
-      </v-layout>
-    </v-container>
+                </v-container>
+              </v-card>
+            </v-flex>
+          </v-layout>
+          <v-layout class="" row wrap>
+            <v-flex md6 xs12>
+              <v-card light>
+                <v-container>
+                  <bar></bar>
+                </v-container>
+              </v-card>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </v-col>
+      <v-col>
+        <Loading
+          v-if="!readyValor"
+          :message="'the service\'s management info'"
+        />
+        <v-container fluid grid-list-md v-else>
+          <v-layout row wrap>
+            <v-flex
+              md3
+              sm6
+              xs12
+              full
+              v-for="(valorizacao, i) in valorizacao"
+              v-bind:key="i"
+            >
+              <v-card :class="valorizacao.bgColor" dark>
+                <v-container fluid grid-list-sm dark>
+                  <v-layout class="mt-0 mb-0 mx-0" row wrap>
+                    <v-flex sm3 hidden-xs-only>
+                      <v-icon class="mx-0" x-large dark>{{
+                        valorizacao.icon
+                      }}</v-icon>
+                    </v-flex>
+                    <v-flex sm9 xs12>
+                      <v-layout class="mt-0 mb-0 pa-0" row wrap>
+                        <v-flex d-flex xs12>
+                          <div class="silver--text subheading">
+                            {{ valorizacao.title }}
+                          </div>
+                        </v-flex>
+                        <v-flex d-flex xs12 class="mx-4">
+                          <div class="silver--text display-1">
+                            {{ valorizacao.data.reduce((a, b) => a + b, 0) }}
+                          </div>
+                          &nbsp;&nbsp;
+                          <v-btn
+                            outlined
+                            class="darkgrey--text darken-1"
+                            v-if="valorizacao.action.label.length > 0"
+                            right
+                            text
+                            small
+                            @click="go(valorizacao.action.link)"
+                          >
+                            {{ valorizacao.action.label }}
+                          </v-btn>
+                        </v-flex>
+                      </v-layout>
+                    </v-flex>
+                  </v-layout>
+                </v-container>
+              </v-card>
+            </v-flex>
+          </v-layout>
+          <v-layout class="" row wrap>
+            <v-flex md6 xs12>
+              <v-card light>
+                <v-container>
+                  <bar-valor></bar-valor>
+                </v-container>
+              </v-card>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </v-col>
+    </v-row>
   </v-content>
 </template>
 
 <script>
 import Loading from "@/components/generic/Loading";
 import Bar from "./chart/Bar";
+import BarValor from "./chart/BarValor";
 
 export default {
   data: () => ({
     stats: [],
+    valorizacao: [],
+    valores: [],
+    totalServices: [],
     number_of_services: -1,
-    ready: false
+    ready: false,
+    readyValor: false
   }),
   components: {
     Loading,
-    Bar
+    Bar,
+    BarValor
   },
   mounted() {
     this.getNumberOfServices();
+    this.getTotalServices();
   },
   methods: {
+    async getTotalServices() {
+      await this.$request("get", "/services")
+        .then(res => {
+          this.totalServices = res.data;
+          for (let i = 0; i < this.totalServices.length; i++) {
+            let servico = this.totalServices[i];
+            if (servico.status >= 2) {
+              this.valores[this.valores.length] = parseInt(
+                servico.orcamento[servico.orcamento.length - 1].value
+              );
+            }
+          }
+          this.valorizacao.push({
+            bgColor: "primary",
+            icon: "euro",
+            title: "Total",
+            data: this.valores,
+            action: {
+              label: "",
+              link: ""
+            }
+          });
+          this.readyValor = true;
+        })
+        .catch(error => alert(error));
+    },
     async getNumberOfServices() {
       await this.$request("get", "/services/monitoring?action=total")
         .then(res => {
---
+++ src/frontend/src/components/services/chart/BarValor.js
@@ -0,0 +1,123 @@
+import { Bar } from "vue-chartjs";
+
+export default {
+  extends: Bar,
+  data() {
+    return {
+      contador: [0,0,0,0,0,0,0],
+      resultsval: [],
+      status_info: [
+        {
+          value: [0, 25],
+          desc: "[0-25]",
+          backgroundColor: "#63b179"
+        },
+        {
+          value: [26, 50],
+          desc: "[26-50]",
+          backgroundColor: "#aed987"
+        },
+        {
+          value: [51, 100],
+          desc: "[51-100]",
+          backgroundColor: "#ffff9d"
+        },
+        {
+          value: [101, 250],
+          desc: "[101-250]",
+          backgroundColor: "#fcc267"
+        },
+        {
+          value: [251, 500],
+          desc: "[251-500]",
+          backgroundColor: "#ef8250"
+        },
+        {
+          value: [501, 1000],
+          desc: "[501-1000]",
+          backgroundColor: "#00b300"
+        },
+        {
+          value: [1001, 9999999999],
+          desc: "[>1000]",
+          backgroundColor: "#00aba9"
+        }
+      ],
+      info: {
+        labels: [],
+        datasets: [
+          {
+            backgroundColor: [],
+            data: []
+          }
+        ]
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: true
+              }
+            }
+          ]
+        },
+        legend: {
+          display: false
+        }
+      }
+    };
+  },
+  mounted() {
+    this.servicesPriceCount();
+  },
+  methods: {
+    async servicesPriceCount() {
+      await this.$request("get", "/services")
+        .then(res => {
+          var result = Object.assign([], res.data).sort(function(a, b) {
+            return a._id - b._id;
+          }).filter(function(a){
+            if (a.status>=2) return a;
+          });
+          this.resultsval=result;
+          result.forEach(element => {
+            var preco = element.orcamento[element.orcamento.length-1].value;
+            if(isNaN(preco)){
+              preco=preco.replace(/\D/g,'');
+            }
+            var descricao = this.status_info.find((s => s.value[0] <= preco) && (s2 => s2.value[1] >= preco)).desc;
+            var cor = this.status_info.find((s => s.value[0] <= preco) && (s2 => s2.value[1] >= preco)).backgroundColor;
+            if(!this.info.labels.includes(descricao)) {
+              this.info.labels.push(descricao);
+              this.info.datasets[0].backgroundColor.push(cor);
+            }
+            if (preco <= 25) {
+              this.contador[0]=this.contador[0]+1;
+            } else if (preco <=50){
+              this.contador[1]=this.contador[1]+1;
+            } else if (preco <=100){
+              this.contador[2]=this.contador[2]+1;
+            } else if (preco <=250){
+              this.contador[3]=this.contador[3]+1;
+            } else if (preco <=500){
+              this.contador[4]=this.contador[4]+1;
+            } else if (preco <=1000){
+              this.contador[5]=this.contador[5]+1;
+            } else {
+              this.contador[6]=this.contador[6]+1;
+            }
+          });
+          this.info.datasets[0].data = this.contador.filter(function(a){
+            return a>0;
+          });  
+          this.renderChart(this.info, this.options);
+        })
+        .catch(err => {
+          return err;
+        });
+    }
+  }
+};
