df3df1977b328eaca6ae39e266a44fe8448aef82
Paulo Barbosa <paulob1992@hotmail.com>
2020-12-08 18:56:51 +0000

    Comecei a parte da review. Falta acabar

--- src/frontend/src/components/services/Biding.vue
+++ src/frontend/src/components/services/Biding.vue
@@ -233,19 +233,283 @@
         </v-stepper-content>
 
         <v-stepper-content step="2">
-          <v-card
-            class="mb-12 elevation-0"
-            color="#E3F2FD"
-            height="200px"
-          ></v-card>
+          
+          <div v-if="10000>this.date_diff" align="center">
+              <v-btn
+                  v-if="this.lastBid != undefined && this.lastBid != this.idLoged"
+                  @click="review()"
+                  color="primary"
+                >
+                  Review Service
+                </v-btn>
+          </div>
+          <div v-else align="center">
+
+              <v-dialog v-model="dialog" width="500">
+              <template v-slot:activator="{ on, attrs }">
+                <v-btn color="red" v-bind="attrs" v-on="on">
+                  Cancel Negotiation
+                </v-btn>
+              </template>
+
+              <v-card>
+                <v-card-title class="headline grey lighten-2">
+                  Are you sure?
+                </v-card-title>
+                <v-card-text>
+                  Are you sure you want to stop negotiation? After this you will
+                  not be able to do more bids and the service will be canceled.
+                </v-card-text>
+
+                <v-divider></v-divider>
+
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn color="primary" text @click="cancelBid(true)">
+                    I Yes
+                  </v-btn>
+                  <v-btn color="primary" text @click="cancelBid(false)">
+                    I No
+                  </v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-dialog>
+          </div>
         </v-stepper-content>
 
         <v-stepper-content step="3">
-          <v-card
-            class="mb-12 elevation-0"
-            color="#E3F2FD"
-            height="200px"
-          ></v-card>
+          <div v-if="this.idLoged != this.data.client">
+            <div>
+              <table>
+                <tr>
+                  <th style="width:100px">
+                    Payment
+                  </th>
+                  <th>
+                    <v-rating
+                      color="blue"
+                      empty-icon="star_outline"
+                      full-icon="star"
+                      half-icon="star_half"
+                      hover
+                      length="5"
+                      size="32"
+                      value="3"
+                    ></v-rating>
+                  </th> 
+                </tr>
+              </table>
+              
+            </div>
+            <div>
+              <table>
+                <tr>
+                  <th style="width:100px">
+                    Punctuality
+                  </th>
+                  <th>
+                    <v-rating
+                      color="blue"
+                      empty-icon="star_outline"
+                      full-icon="star"
+                      half-icon="star_half"
+                      hover
+                      length="5"
+                      size="32"
+                      value="3"
+                    ></v-rating>
+                  </th> 
+                </tr>
+              </table>
+              
+            </div>
+            <div>
+              <table>
+                <tr>
+                  <th style="width:100px">
+                    Security
+                  </th>
+                  <th>
+                    <v-rating
+                      color="blue"
+                      empty-icon="star_outline"
+                      full-icon="star"
+                      half-icon="star_half"
+                      hover
+                      length="5"
+                      size="32"
+                      value="3"
+                    ></v-rating>
+                  </th> 
+                </tr>
+              </table>
+              
+            </div>
+            <div>
+              <table >
+                <tr>
+                  <th style="width:100px">
+                    General
+                  </th>
+                  <th>
+                    <v-rating
+                      color="blue"
+                      empty-icon="star_outline"
+                      full-icon="star"
+                      half-icon="star_half"
+                      hover
+                      length="5"
+                      size="32"
+                      value="3"
+                    ></v-rating>
+                  </th> 
+                </tr>
+              </table>
+              
+            </div>
+            <v-textarea
+              solo
+              name="input-7-4"
+              label="Commentary"
+            ></v-textarea>
+            <div align="center">
+              <v-btn
+                  v-if="this.lastBid != undefined && this.lastBid != this.idLoged"
+                  @click="sendReview()"
+                  color="primary"
+                >
+                  Send Review
+                </v-btn>
+            </div>
+            
+          </div>
+          <div v-else>
+            <div>
+              <table>
+                <tr>
+                  <th style="width:100px">
+                    Punctuality
+                  </th>
+                  <th>
+                    <v-rating
+                      color="blue"
+                      empty-icon="star_outline"
+                      full-icon="star"
+                      half-icon="star_half"
+                      hover
+                      length="5"
+                      size="32"
+                      value="3"
+                    ></v-rating>
+                  </th> 
+                </tr>
+              </table>
+              
+            </div>
+            <div>
+              <table>
+                <tr>
+                  <th style="width:100px">
+                    Quality
+                  </th>
+                  <th>
+                    <v-rating
+                      color="blue"
+                      empty-icon="star_outline"
+                      full-icon="star"
+                      half-icon="star_half"
+                      hover
+                      length="5"
+                      size="32"
+                      value="3"
+                    ></v-rating>
+                  </th> 
+                </tr>
+              </table>
+              
+            </div>
+            <div>
+              <table>
+                <tr>
+                  <th style="width:100px">
+                    Security
+                  </th>
+                  <th>
+                    <v-rating
+                      color="blue"
+                      empty-icon="star_outline"
+                      full-icon="star"
+                      half-icon="star_half"
+                      hover
+                      length="5"
+                      size="32"
+                      value="3"
+                    ></v-rating>
+                  </th> 
+                </tr>
+              </table>
+              
+            </div>
+            <div>
+              <table >
+                <tr>
+                  <th style="width:100px">
+                    Attendance
+                  </th>
+                  <th>
+                    <v-rating
+                      color="blue"
+                      empty-icon="star_outline"
+                      full-icon="star"
+                      half-icon="star_half"
+                      hover
+                      length="5"
+                      size="32"
+                      value="3"
+                    ></v-rating>
+                  </th> 
+                </tr>
+              </table>
+              
+            </div>
+            <div>
+              <table >
+                <tr>
+                  <th style="width:100px">
+                    General
+                  </th>
+                  <th>
+                    <v-rating
+                      color="blue"
+                      empty-icon="star_outline"
+                      full-icon="star"
+                      half-icon="star_half"
+                      hover
+                      length="5"
+                      size="32"
+                      value="3"
+                    ></v-rating>
+                  </th> 
+                </tr>
+              </table>
+              
+            </div>
+            <v-textarea
+              solo
+              name="input-7-4"
+              label="Commentary"
+            ></v-textarea>
+            <div align="center">
+              <v-btn
+                  v-if="this.lastBid != undefined && this.lastBid != this.idLoged"
+                  @click="sendReview()"
+                  color="primary"
+                >
+                  Send Review
+                </v-btn>
+            </div>
+            
+          </div>
         </v-stepper-content>
         <v-stepper-content step="4">
           <v-card class="mb-12 elevation-0" color="#E3F2FD" height="200px"
@@ -296,6 +560,42 @@ export default {
   },
 
   methods: {
+    //10000 equivale a 1 dia
+    date_difference(data, horario){
+    console.log("Entrou")
+	if(data === undefined){
+		return -1;
+	} else if (horario === undefined){
+		horario = "00:00:00";
+	} else {
+		horario = horario + ":00";
+	}
+	let marcacao = new Date(data + "T" + horario + "Z");
+
+	// current date
+	let date_ob = new Date();
+
+	return this.date_to_number(marcacao) - this.date_to_number(date_ob); 
+  },
+    date_to_number(date_ob){
+	// adjust 0 before single digit date
+	let date = ("0" + date_ob.getDate()).slice(-2);
+
+	// current month
+	let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+
+	// current year
+	let year = date_ob.getFullYear();
+
+	// current hours
+	let hours = date_ob.getHours();
+
+	// current minutes
+	let minutes = date_ob.getMinutes();
+
+	// YYYYMMDDHHmm
+	return minutes + hours * 100 + date * 10000 + month * 1000000 + year * 100000000;
+  },
     async comment() {
       var value = this.input;
 
@@ -308,7 +608,6 @@ export default {
         }
       );
       this.input = null;
-
       this.lastBid = this.idLoged;
 
       await this.getService();
@@ -327,7 +626,15 @@ export default {
     },
     async getService() {
       var res = await this.$request("get", "/services/" + this.idService);
+      
       this.data = res.data;
+
+      //testar datas
+      this.date_diff = this.date_difference("2020-12-08");
+      
+      //this.date_diff = this.date_difference(res.data.date,res.data.hour);
+      
+      
       //Vai ordenar orçamentos por data
       this.data.orcamento.sort((a, b) => (a.datetime < b.datetime ? 1 : -1));
       //vai usar isto para escolher a tab em que esta negotiation / waiting service etc
@@ -377,6 +684,19 @@ export default {
         this.dialog = false;
       } else {
         this.dialog = false;
+      }
+    },
+    review() {
+      this.e1=3;
+    },
+    sendReview() {
+      //se for o cliente online
+      if (this.idLoged==this.data.client){
+          //ir buscar os dados usando o findelement by id, tenho de por ID's em todos os elementos ainda
+      }
+      //se for a empresa
+      else{
+        //ir buscar os dados usando o findelement by id, tenho de por ID's em todos os elementos ainda
       }
     }
   }
