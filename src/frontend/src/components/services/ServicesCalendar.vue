77fd9fc89ebde98cc28ab87343dafbea7281bdfc
citoplasme <jony.pi10@gmail.com>
2020-12-10 16:24:31 +0000

    :)

--- src/frontend/src/components/services/ServicesCalendar.vue
+++ src/frontend/src/components/services/ServicesCalendar.vue
@@ -385,7 +385,7 @@ export default {
       var events = [];
 
       // Filtrar serviços com prestadores apenas e que tenham data e hora -> Para o calendário só
-      snapshot.data = snapshot.data.filter(el => el.service_provider !== undefined && el.date !== undefined && el.hour.hour !== undefined);
+      snapshot.data = snapshot.data.filter(el => el.service_provider !== undefined && el.date !== undefined && el.hour !== undefined);
       
       snapshot.data.forEach(async element => {
         //Vai buscar o nome da empresa
