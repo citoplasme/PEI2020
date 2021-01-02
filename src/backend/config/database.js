d676f34b88b32a510377cd5e8cd0ae6f45266d9e
Paulo-B <a81139@alunos.uminho.pt>
2021-01-02 14:36:28 +0000

    Swagger-nginx fixed

--- src/backend/config/database.js
+++ src/backend/config/database.js
@@ -1,10 +1,10 @@
-const ip = process.env.IP || 'localhost' // IP da máquina onde corre
-const port = process.env.PORT || '7779'
+const ip = process.env.IP // IP da máquina onde corre
+const port = process.env.PORT
 module.exports.apiVersion = process.env.API_VERSION || 'v1'
 
 module.exports.host = 'http://'+ip+':'+port
 
-module.exports.swaggerURL = process.env.SWAGGER_URL || this.host //  || 'http://' + ip + ":" + this.port 
+module.exports.swaggerURL = process.env.SWAGGER_URL //  || 'http://' + ip + ":" + this.port
 
 module.exports.userDB = process.env.MONGODB ? 'mongodb://ADMIN:ADMIN@' + process.env.MONGODB + '/authtemplate' : 'mongodb://localhost/authtemplate'
 
